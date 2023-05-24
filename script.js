3243cf0751ad18dac7bc908e8c35f97e6b1ee5b2
ashutosh-karn <karnashu23@gmail.com>
2023-05-24 19:17:45 +0530

    formatted (prettier)

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 function changeImageAndColor(buttonColor, bgColor, imageLocation) {
-  const imageElement = document.getElementById('image');
+  const imageElement = document.getElementById("image");
   imageElement.src = imageLocation;
-  
-  const uploadButton = document.getElementById('upload-button');
+
+  const uploadButton = document.getElementById("upload-button");
   uploadButton.style.backgroundColor = buttonColor;
-  
+
   document.body.style.backgroundColor = bgColor;
 }
 
 function handleUploadButtonClick() {
-  const uploadInput = document.getElementById('upload-input');
+  const uploadInput = document.getElementById("upload-input");
   uploadInput.click();
 }
 
 function handleFileUpload(event) {
   const file = event.target.files[0];
-  
-  if (file && (file.type === 'image/jpeg' || file.type === 'image/png') && file.size <= 5242880) {
+
+  if (
+    file &&
+    (file.type === "image/jpeg" || file.type === "image/png") &&
+    file.size <= 5242880
+  ) {
     const reader = new FileReader();
-    
-    reader.onload = function() {
-      const uploadedImage = document.createElement('img');
+
+    reader.onload = function () {
+      const uploadedImage = document.createElement("img");
       uploadedImage.src = reader.result;
-      uploadedImage.classList.add('uploaded-image');
-      
-      setTimeout(function() {
+      uploadedImage.classList.add("uploaded-image");
+
+      setTimeout(function () {
         document.body.appendChild(uploadedImage);
       }, 2000);
-    }
-    
+    };
+
     reader.readAsDataURL(file);
   }
-}    
-const uploadInput = document.getElementById('upload-input');
-uploadInput.addEventListener('change', handleFileUpload);
-
+}
+const uploadInput = document.getElementById("upload-input");
+uploadInput.addEventListener("change", handleFileUpload);
